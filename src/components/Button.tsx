fd3dd81af889c0ea43afa9b1d2f034a25b619255
DesiZafirova <desszafirowa@gmail.com>
2025-01-22 00:06:23 +0200

    Button component built

---
+++ src/components/Button.tsx
@@ -0,0 +1,15 @@
+import { ComponentPropsWithoutRef } from 'react';
+
+type ButtonProps = ComponentPropsWithoutRef<'button'> & { href?: never };
+
+type LinkProps = ComponentPropsWithoutRef<'a'> & { href: string };
+
+function isAnchorProps(props: ButtonProps | LinkProps): props is AnchorProps {
+  return 'href' in props;
+}
+
+export default function Button(props: ButtonProps | LinkProps) {
+  if (isAnchorProps(props)) return <a className="button" {...props}></a>;
+
+  return <button className="button" {...props}></button>;
+}
