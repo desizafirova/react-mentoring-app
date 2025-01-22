78c4ab625877acc37582282a5d35f8b9cba92de4
DesiZafirova <desszafirowa@gmail.com>
2025-01-22 16:21:19 +0200

    uninstall tailwindcss

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import Navigation from './Navigation';
 
 export default function Header() {
   return (
-    <header>
+    <header className="flex">
       <h2>ReactMentoring</h2>
       <Navigation />
     </header>
