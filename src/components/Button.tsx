9f6874e817574d59c2056e1bd7f5788b382bc744
DesiZafirova <desszafirowa@gmail.com>
2025-01-22 09:56:21 +0200

    adding textOnly prop

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,15 +1,32 @@
 import { ComponentPropsWithoutRef } from 'react';
 
-type ButtonProps = ComponentPropsWithoutRef<'button'> & { href?: never };
+type ButtonProps = ComponentPropsWithoutRef<'button'> & {
+  textOnly: boolean;
+  href?: never;
+};
 
-type LinkProps = ComponentPropsWithoutRef<'a'> & { href: string };
+type LinkProps = ComponentPropsWithoutRef<'a'> & {
+  textOnly: boolean;
+  href: string;
+};
 
 function isAnchorProps(props: ButtonProps | LinkProps): props is LinkProps {
   return 'href' in props;
 }
 
 export default function Button(props: ButtonProps | LinkProps) {
-  if (isAnchorProps(props)) return <a className="button" {...props}></a>;
+  if (isAnchorProps(props))
+    return (
+      <a
+        className={`button ${props.textOnly && `button--text-only`}`}
+        {...props}
+      ></a>
+    );
 
-  return <button className="button" {...props}></button>;
+  return (
+    <button
+      className={`button ${props.textOnly && `button--text-only`}`}
+      {...props}
+    ></button>
+  );
 }
